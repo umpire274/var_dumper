4b90d54e27897b149d7de85f31ea292dddff18bf
Alessandro Maestri <umpire274@303487.no-reply.drupal.org>
2021-08-04 10:30:50 +0200

    Created test file for udump function

---
+++ tests/Command/VarDumperTest.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace Command;
+
+use Hamcrest\Util;
+use PHPMetarTafParser\Parser\MetarParser;
+use stdClass;
+use var_dumper\Command\VarDumper;
+use PHPUnit\Framework\TestCase;
+
+class VarDumperTest extends TestCase {
+
+	private $vdump;
+
+	public function setUp(): void {
+		parent ::setUp();
+		$this -> vdump = new VarDumper();
+		Util ::registerGlobalFunctions();
+	}
+
+	public function testDump() {
+
+		$myobject = new stdClass();
+		$myobject -> myvalue = 'Hello World';
+		$myobject -> ar = [
+			"ab" => [
+				"ab-1" => [
+					"ab-1-a" => "a",
+					"ab-1-b" => "b",
+				],
+				"ab-2" => 2,
+			],
+			"cd" => 'ciao broccoletto fritto',
+		];
+
+		$dbg = $this -> vdump -> udump( $myobject, true, '', true );
+
+		print ($dbg);
+
+		self ::assertNotNull( $dbg );
+	}
+
+}
